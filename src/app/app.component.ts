c16a4004b3aaa1c8db7a110b88d72dfae481bddc
Celina Sehic <cel.sehic@gmail.com>
2023-01-04 16:59:45 +0100

    adds auth0 login to navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import { AuthService } from '@auth0/auth0-angular';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +9,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'palpool';
+  constructor(public auth: AuthService) {}
 }
